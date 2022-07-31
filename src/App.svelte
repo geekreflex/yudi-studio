b466e4e38b4b686c71d747ac9fe862fb9dd83717
jerry <jerrynwosu007@gmail.com>
2022-07-31 05:15:30 +0100

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,25 @@
 <script>
-import svelteLogo from "./assets/svelte.svg";
-import Counter from "./lib/Counter.svelte";
+import { fabric } from "fabric";
+import { onMount } from "svelte";
+import Controls from "./components/Controls.svelte";
+import { editor } from "./store/store";
+
+onMount(() => {
+  const cv = new fabric.Canvas("canvas-elem");
+  editor.set(cv);
+});
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <Controls />
+  <canvas id="canvas-elem" width="500" height="500"></canvas>
 </main>
 
 <style>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+main {
+  margin: 50px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.svelte:hover {
-  filter: drop-shadow(0 0 2em #ff3e00aa);
-}
-.read-the-docs {
-  color: #888;
+canvas {
+  border: 3px solid #333;
 }
 </style>
---
+++ src/components/Controls.svelte
@@ -0,0 +1,17 @@
+<script>
+import { fabric } from "fabric";
+import { editor } from "../store/store";
+
+const onCircle = () => {
+  const circle = new fabric.Circle({
+    fill: "blue",
+    radius: 50,
+  });
+  $editor.add(circle);
+  $editor.renderAll();
+};
+</script>
+
+<main>
+  <button on:click="{onCircle}">Add Circle</button>
+</main>
