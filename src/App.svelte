5c79e6c71221f83786eb710ba59d2bb351c049d1
jerry <jerrynwosu007@gmail.com>
2022-07-31 05:32:54 +0100

    basic render

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,17 @@
 <script>
-import { fabric } from "fabric";
-import { onMount } from "svelte";
+import Canvas from "./components/Canvas.svelte";
 import Controls from "./components/Controls.svelte";
-import { editor } from "./store/store";
-
-onMount(() => {
-  const cv = new fabric.Canvas("canvas-elem");
-  editor.set(cv);
-});
 </script>
 
 <main>
   <Controls />
-  <canvas id="canvas-elem" width="500" height="500"></canvas>
+  <Canvas />
 </main>
 
 <style>
 main {
-  margin: 50px;
-}
-canvas {
-  border: 3px solid #333;
+  background-color: #ccc;
+  width: 100%;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Canvas.svelte
@@ -0,0 +1,37 @@
+<script>
+import { fabric } from "fabric";
+import { onMount } from "svelte";
+import { editor } from "../store/store";
+
+onMount(() => {
+  const canvas = new fabric.Canvas("canvas-elem", {
+    backgroundColor: "#fff",
+  });
+  editor.set(canvas);
+
+  setCurrentDimension(canvas);
+});
+
+function setCurrentDimension(canvas) {
+  let canvasParent = document.querySelector(".canvas-wrap");
+  canvas.setHeight(canvasParent.clientHeight || 0);
+  canvas.setWidth(canvasParent.clientWidth || 0);
+}
+</script>
+
+<main>
+  <div class="canvas-wrap">
+    <canvas id="canvas-elem"></canvas>
+  </div>
+</main>
+
+<style>
+.canvas-wrap {
+  display: flex;
+  height: 500px;
+  width: 500px;
+}
+canvas {
+  box-shadow: 0 4px 9px #888;
+}
+</style>
