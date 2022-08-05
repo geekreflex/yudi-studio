001eacb28fad5a99d5170162e5a9881c3cbe2a70
jerry <jerrynwosu007@gmail.com>
2022-08-05 01:27:51 +0100

    preview for publish

---
+++ src/components/SmallPreview.svelte
@@ -0,0 +1,16 @@
+<script></script>
+
+<main>
+  <label for="preview">Preview</label>
+  <div id="preview" class="preview"></div>
+</main>
+
+<style>
+.preview {
+  width: 300px;
+  border: 1px solid #555;
+  border-radius: 6px;
+  min-height: 200px;
+  cursor: pointer;
+}
+</style>
